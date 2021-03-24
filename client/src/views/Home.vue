ef776d38177b64b1a0835927fb1f92d04d5fba73
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-03-24 22:26:42 +0100

    first commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,67 @@
+<!-- <template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+-->
+
+
+<template>
+  <div>
+    <hr>
+    <div class="list-unstyled" v-for="message in messages" :key="message._id">
+      <li class="media">
+        <img v-if="message.imageURL" class="mr-3" :src="message.imageURL" :alt="message.subject">
+        <div class="media-body">
+          <h4 class="mt-0 mb-1">{{message.username}}</h4>
+          <h5 class="mt-0 mb-1">{{message.subject}}</h5>
+          {{message.message}}
+          <br />
+          <small>{{message.created}}</small>
+        </div>
+      </li>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+const API_URL = "http://localhost:4000/testCollection";
+
+export default {
+  name: "home",
+  data: () => ({
+    error: "",
+    messages: []
+  }),
+
+  mounted() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(result => {
+        this.messages = result;
+      });
+  },
+  methods: {}
+};
+</script>
+
+<style>
+img {
+  max-width: 300px;
+  height: auto;
+}
+</style>
