d950443eaea38dba7067009fe9c538d71522df94
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-04-21 14:29:43 +0200

    finishing getHeroesByNameFilter controller action

--- src/controllers/heroController.js
+++ src/controllers/heroController.js
@@ -27,7 +27,9 @@ export const getHeroByID = async (req, res) => {
   // }
 };
 
-export const getHeroesByNameFilter = async (req, res) => {
+export const getHeroesByNameFilter = (req, res) => {
+  //requests all 3 Apis and searches for the heroName
+
   const heroName = req.params.heroName;
   const timestamp = String(Date.now());
   const privateKey = credentials.marvel_api.private_key;
@@ -41,32 +43,52 @@ export const getHeroesByNameFilter = async (req, res) => {
   const superheroes_api_request_url = `https://superheroapi.com/api/${credentials.superhero_api.access_token}/search/${heroName}`;
   const comicvines_api_request_url = `http://www.comicvine.com/api/search/?api_key=${credentials.comic_vines_api.access_token}&query=${heroName}&resources=character&format=json`;
 
-  let promise1 = axios.get(marvel_api_request_url);
-  let promise2 = axios.get(superheroes_api_request_url);
-  let promise3 = axios.get(comicvines_api_request_url);
+  const promises = [
+    axios.get(marvel_api_request_url),
+    axios.get(superheroes_api_request_url),
+    axios.get(comicvines_api_request_url),
+  ];
 
-  Promise.all([promise1, promise2, promise3])
-    .then((results) => {
-      //executed when all APIs sent data back
-      console.log("THEN");
-      let a = results.map((apiResponse) => {
-        return apiResponse.data.results || apiResponse.data.data.results || [];
+  Promise.allSettled(promises) //returns a promise that resolves after all of the given promises have either fulfilled or rejected, with an array of objects that each describes the outcome of each promise.
+    .then((apiResponses) => {
+      //gets executed when all promises have either fulfilled or rejected
+
+      const filteredResponse = apiResponses
+        .map((apiResponse) => apiResponse.value)
+        .filter((item) => item !== undefined)
+        .filter((item) => item.data.response !== "error");
+
+      const responseObject = {};
+
+      filteredResponse.forEach((item) => {
+        switch (item.config.url) {
+          case marvel_api_request_url:
+            Object.defineProperty(responseObject, "Marvel-API", {
+              value: item.data.data.results,
+              enumerable: true,
+            });
+            break;
+          case superheroes_api_request_url:
+            Object.defineProperty(responseObject, "Superheroes-API", {
+              value: item.data.results,
+              enumerable: true,
+            });
+            break;
+          case comicvines_api_request_url:
+            Object.defineProperty(responseObject, "Comicvine-API", {
+              value: item.data.results,
+              enumerable: true,
+            });
+            break;
+          default:
+            throw new Error("API request not defined");
+        }
       });
-      console.log(a);
-      res.json(a);
-    })
-    .catch((err) => {
-      console.log("ERR");
-      //executed when at least one of the promises throw an error
-      console.log(err);
-    })
-    .finally(() => {
-      console.log(promise1);
-      console.log(promise2);
-      console.log(promise3);
-      console.log("FINALLY");
-      //executed when the promises are resolved
+
+      res.json(responseObject);
     });
+  // ToDo: Handle Promise Timeout. What if the API-Calls take 5 hours?
+  // ToDo: Handle Promise Timeout on frontend
 };
 
 export const getRandomHero = (req, res) => {};
