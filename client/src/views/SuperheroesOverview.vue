baef7b46a049d71b99a6d0add7eb3b64b32d9c24
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-04-21 14:51:07 +0200

    adding simple frontend to send axios requests

--- client/src/views/SuperheroesOverview.vue
+++ client/src/views/SuperheroesOverview.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
     <h1>Superheroes Overview</h1>
+    <div>
+      <input type="text" v-model="inputText" />
+      <button @click="getHeroesByName">Send Axios Request</button>
+    </div>
+    <div>
+      {{ superheroesJSON }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SuperheroesOverview",
   components: {},
   props: {},
   watch: {},
+  data() {
+    return {
+      superheroesJSON: {},
+      inputText: "",
+    };
+  },
 
   mounted() {},
-  methods: {},
+  methods: {
+    async getHeroesByName() {
+      try {
+        const response = await axios.get(`/api/hero/name/${this.inputText}`);
+        this.superheroesJSON = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
 
