d6d045977a9966bdd059894d1cee629aa5d352ed
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-04-29 18:24:30 +0200

    swaping new controller action

--- src/controllers/heroController.js
+++ src/controllers/heroController.js
@@ -3,59 +3,79 @@ import credentials from "../../api_credentials";
 import crypto from "crypto";
 
 export const getHeroByID = async (req, res) => {
-  var heroID = req.params.heroID;
-  var id = heroID.charAt(0);
-  const finalHeroID = heroID.slice(3);
-  const timestamp = String(Date.now());
-  const privateKey = credentials.marvel_api.private_key;
-  const publicKey = credentials.marvel_api.public_key;
-  const hash = crypto
-    .createHash("md5")
-    .update(timestamp + privateKey + publicKey)
-    .digest("hex");
+  const heroID = req.params.heroID;
 
-  if (id == 0) {
-    //marvel api
-    try {
-      const response = await axios.get(
-        `http://gateway.marvel.com/v1/public/characters/${finalHeroID}?apikey=${publicKey}&limit=100&ts=${timestamp}&hash=${hash}`
-      );
-      const responseArray = [];
+  try {
+    const response = await axios.get(
+      `https://comicvine.gamespot.com/api/character/4005-${heroID}/?api_key=${credentials.comic_vines_api.access_token}&format=json`
+    );
+    const responseArray = [];
 
-      responseArray.push({
-        id: response.data.results.id,
-        name: response.data.results.name,
-        imageURL: response.data.results.thumbnail.path,
-      });
+    responseArray.push({
+      id: response.data.results.id,
+      name: response.data.results.name,
+      imageURL: response.data.results.image.medium_url,
+    });
 
-      res.json(responseArray);
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    //superhero api
-    try {
-      const response = await axios.get(
-        `https://superheroapi.com/api/${credentials.superhero_api.access_token}/${finalHeroID}`
-      );
-      const responseArray = [];
-
-      responseArray.push({
-        id: response.data.id,
-        name: response.data.name,
-        imageURL: response.data.image.url,
-      });
-
-      res.json(responseArray);
-    } catch (error) {
-      console.error(error);
-    }
+    res.json(responseArray);
+  } catch (error) {
+    console.error(error);
   }
+
+  // var heroID = req.params.heroID;
+  // var id = heroID.charAt(0);
+  // const finalHeroID = heroID.slice(3);
+  // const timestamp = String(Date.now());
+  // const privateKey = credentials.marvel_api.private_key;
+  // const publicKey = credentials.marvel_api.public_key;
+  // const hash = crypto
+  //   .createHash("md5")
+  //   .update(timestamp + privateKey + publicKey)
+  //   .digest("hex");
+
+  // if (id == 0) {
+  //   //marvel api
+  //   try {
+  //     const response = await axios.get(
+  //       `http://gateway.marvel.com/v1/public/characters/${finalHeroID}?apikey=${publicKey}&limit=100&ts=${timestamp}&hash=${hash}`
+  //     );
+  //     const responseArray = [];
+
+  //     responseArray.push({
+  //       id: response.data.results.id,
+  //       name: response.data.results.name,
+  //       imageURL: response.data.results.thumbnail.path,
+  //     });
+
+  //     res.json(responseArray);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // } else {
+  //   //superhero api
+  //   try {
+  //     const response = await axios.get(
+  //       `https://superheroapi.com/api/${credentials.superhero_api.access_token}/${finalHeroID}`
+  //     );
+  //     const responseArray = [];
+
+  //     responseArray.push({
+  //       id: response.data.id,
+  //       name: response.data.name,
+  //       imageURL: response.data.image.url,
+  //     });
+
+  //     res.json(responseArray);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
   //http://gateway.marvel.com/v1/public/characters/${characterID}?apikey=${publicKey}&limit=100&ts=${timestamp}&hash=${hash}`;
   //https://superheroapi.com/api/${access-token}/${characterID}
   //https://comicvine.gamespot.com/api/character/4005-${characterID}/?api_key=${access-token}&format=json
 };
 
+/*
 export const getHeroesByNameFilter = (req, res) => {
   //requests all 3 Apis and searches for the heroName
 
@@ -146,6 +166,76 @@ export const getHeroesByNameFilter = (req, res) => {
       });
 
       res.json(responseArray);
+    });
+  // ToDo: Handle Promise Timeout. What if the API-Calls take 5 hours?
+  // ToDo: Handle Promise Timeout on frontend
+
+
+
+
+
+
+};
+*/
+
+export const getHeroesByNameFilter = (req, res) => {
+  //requests all 3 Apis and searches for the heroName
+
+  const heroName = req.params.heroName;
+  // const timestamp = String(Date.now());
+  // const privateKey = credentials.marvel_api.private_key;
+  // const publicKey = credentials.marvel_api.public_key;
+  // const hash = crypto
+  //   .createHash("md5")
+  //   .update(timestamp + privateKey + publicKey)
+  //   .digest("hex");
+
+  //const marvel_api_request_url = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${heroName}&orderBy=name&limit=100&ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+  // Return characters with names that begin with the specified string (e.g. Sp) for Spider-Man.
+  // order by name ascending
+  // limit 100: returns not more than 100 items, max: 100
+
+  // const superheroes_api_request_url = `https://superheroapi.com/api/${credentials.superhero_api.access_token}/search/${heroName}`;
+  //gibt unendlich viele zurück
+  //const comicvines_api_request_url = `http://www.comicvine.com/api/search/?api_key=${credentials.comic_vines_api.access_token}&query=${heroName}&resources=character&format=json`;
+  // const comicvines_api_request_url = `https://comicvine.gamespot.com/api/characters/?api_key=${credentials.comic_vines_api.access_token}&filter=name:${heroName}&field_list=id,name,image,api_detail_url&format=json`;
+
+  const comicvines_MARVEL_request_url = `https://comicvine.gamespot.com/api/publisher/31/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
+  const comicvines_DC_request_url = `https://comicvine.gamespot.com/api/publisher/10/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
+  // max 100 results
+  //&field_list=id,name,image,api_detail_url   ---> works!
+  //&limit=100
+  //&sort=name:asc
+  //&filter=name:spider
+
+  const promises = [
+    // axios.get(marvel_api_request_url),
+    // axios.get(superheroes_api_request_url),
+    axios.get(comicvines_DC_request_url),
+    axios.get(comicvines_MARVEL_request_url),
+  ];
+
+  //liefern 39.181 Characters von DC und Marvel zurück
+
+  Promise.allSettled(promises) //returns a promise that resolves after all of the given promises have either fulfilled or rejected, with an array of objects that each describes the outcome of each promise.
+    .then((apiResponses) => {
+      //gets executed when all promises have either fulfilled or rejected
+
+      const responseArray = apiResponses
+        .filter((apiResponse) => apiResponse.status !== "rejected") //filter out rejected responses
+        .map((apiResponse) => apiResponse.value.data.results.characters); //show only the characters
+
+      const generatedResponse = [...responseArray[0], ...responseArray[1]] //create one Array with all characters in it
+        .map((character) => {
+          return { id: character.id, name: character.name }; //show only id and name of characters
+        })
+        //ToDo: Die API-Calls hier sofort cachen!!!!
+        .filter(
+          (character) =>
+            character.name.toLowerCase().includes(heroName.toLowerCase()) //filter out heroes that include not the name the user typed in.
+        );
+
+      res.json(generatedResponse);
     });
   // ToDo: Handle Promise Timeout. What if the API-Calls take 5 hours?
   // ToDo: Handle Promise Timeout on frontend
