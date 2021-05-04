ff41a8b76f68b3d0f4b8606ec80f3f5585219eef
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-05-04 10:38:15 +0200

    enabling caching in heroController

--- src/controllers/heroController.js
+++ src/controllers/heroController.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import credentials from "../../api_credentials";
 import crypto from "crypto";
+import NodeCache from "node-cache";
 
-export const getHeroByID = async (req, res) => {
+const myCache = new NodeCache();
+const cacheTTL = 60; //60 Minuten
+
+export const getHeroByID = (req, res) => {
   const heroID = req.params.heroID;
   const timestamp = String(Date.now());
   const privateKey = credentials.marvel_api.private_key;
@@ -70,54 +74,55 @@ export const getHeroByID = async (req, res) => {
 
 export const getHeroesByNameFilter = (req, res) => {
   const heroName = req.params.heroName;
+  const cachedHeroesList = myCache.get("HeroesList");
 
-  //const marvel_api_request_url = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${heroName}&orderBy=name&limit=100&ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
-  // Return characters with names that begin with the specified string (e.g. Sp) for Spider-Man.
-  // order by name ascending
-  // limit 100: returns not more than 100 items, max: 100
+  if (cachedHeroesList) {
+    //This is executed when the cache is available
 
-  // const superheroes_api_request_url = `https://superheroapi.com/api/${credentials.superhero_api.access_token}/search/${heroName}`;
-  //gibt unendlich viele zurück
-  //const comicvines_api_request_url = `http://www.comicvine.com/api/search/?api_key=${credentials.comic_vines_api.access_token}&query=${heroName}&resources=character&format=json`;
-  // const comicvines_api_request_url = `https://comicvine.gamespot.com/api/characters/?api_key=${credentials.comic_vines_api.access_token}&filter=name:${heroName}&field_list=id,name,image,api_detail_url&format=json`;
+    //filter out heroes that include not the name the user typed in.
+    const cachedResponse = cachedHeroesList.data.filter((character) =>
+      character.name.toLowerCase().includes(heroName.toLowerCase())
+    );
 
-  const comicvines_MARVEL_request_url = `https://comicvine.gamespot.com/api/publisher/31/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
-  const comicvines_DC_request_url = `https://comicvine.gamespot.com/api/publisher/10/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
-  // max 100 results
-  //&field_list=id,name,image,api_detail_url   ---> works!
-  //&limit=100
-  //&sort=name:asc
-  //&filter=name:spider
+    res.json(cachedResponse);
+    myCache.ttl("HeroesList", cacheTTL); //renew cache TTL
+  } else {
+    //This is executed when no cache is available
 
-  const promises = [
-    // axios.get(marvel_api_request_url),
-    // axios.get(superheroes_api_request_url),
-    axios.get(comicvines_DC_request_url),
-    axios.get(comicvines_MARVEL_request_url),
-  ];
+    //get data from APIs
+    const comicvines_MARVEL_request_url = `https://comicvine.gamespot.com/api/publisher/31/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
+    const comicvines_DC_request_url = `https://comicvine.gamespot.com/api/publisher/10/?api_key=${credentials.comic_vines_api.access_token}&field_list=characters&format=json`;
 
-  //liefern 39.181 Characters von DC und Marvel zurück
+    //fetch 39.181 characters from DC and Marvel
+    const promises = [
+      axios.get(comicvines_DC_request_url),
+      axios.get(comicvines_MARVEL_request_url),
+    ];
 
-  Promise.allSettled(promises) //returns a promise that resolves after all of the given promises have either fulfilled or rejected, with an array of objects that each describes the outcome of each promise.
-    .then((apiResponses) => {
-      //gets executed when all promises have either fulfilled or rejected
+    Promise.allSettled(promises) //returns a promise that resolves after all of the given promises have either fulfilled or rejected, with an array of objects that each describes the outcome of each promise.
+      .then((apiResponses) => {
+        //gets executed when all promises have either fulfilled or rejected
 
-      const responseArray = apiResponses
-        .filter((apiResponse) => apiResponse.status !== "rejected") //filter out rejected responses
-        .map((apiResponse) => apiResponse.value.data.results.characters); //show only the characters
+        const responseArray = apiResponses
+          .filter((apiResponse) => apiResponse.status !== "rejected") //filter out rejected responses
+          .map((apiResponse) => apiResponse.value.data.results.characters); //show only the characters
 
-      const generatedResponse = [...responseArray[0], ...responseArray[1]] //create one Array with all characters in it
-        .map((character) => {
-          return { id: character.id, name: character.name }; //show only id and name of characters
-        })
-        //ToDo: Die API-Calls hier sofort cachen!!!!
-        .filter(
+        const generatedResponse = [...responseArray[0], ...responseArray[1]] //create one Array with all characters in it
+          .map((character) => {
+            return { id: character.id, name: character.name }; //show only id and name of characters
+          });
+
+        myCache.set("HeroesList", { data: generatedResponse }, cacheTTL);
+
+        const generatedFilteredResponse = generatedResponse.filter(
+          //filter out heroes that include not the name the user typed in.
           (character) =>
-            character.name.toLowerCase().includes(heroName.toLowerCase()) //filter out heroes that include not the name the user typed in.
+            character.name.toLowerCase().includes(heroName.toLowerCase())
         );
 
-      res.json(generatedResponse);
-    });
+        res.json(generatedFilteredResponse);
+      });
+  }
   // ToDo: Handle Promise Timeout. What if the API-Calls take 5 hours?
   // ToDo: Handle Promise Timeout on frontend
 };
