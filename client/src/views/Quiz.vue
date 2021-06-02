1d4415a5a203f3a841fb2c9c7d0247608f6292ab
ReginaSchrattmaier <32732979+ReginaSchrattmaier@users.noreply.github.com>
2021-06-02 14:52:04 +0200

    handle quiz results, add basic styling

--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="quiz">
-    <h1>{{ title }}</h1>
+  <div id="quiz" class="w-full md:max-w-7xl">
+    <div class="p-4 bg-red-500 headline">
+      <h1 class="uppercase inline-block text-2xl text-white mt-3">
+        {{ title }}
+      </h1>
+    </div>
     <div v-if="started">
       <div v-for="(question, index) in questions" :key="question.id">
         <div v-show="index === questionIndex">
           <h2>{{ question.text }}</h2>
           <ol>
-            <li v-for="response in question.responses" :key="response.id">
+            <li
+              v-for="response in question.responses"
+              :key="response.id"
+              class="border border-red-500 py-2 px-4 input-field rounded-full mt-4 mb-4"
+            >
               <label>
                 <input
                   type="radio"
@@ -18,17 +26,49 @@
               </label>
             </li>
           </ol>
-          <button v-if="questionIndex > 0" v-on:click="prev">prev</button>
-          <button v-on:click="next">next</button>
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white py-3 px-4 rounded mr-4"
+            v-if="questionIndex > 0"
+            v-on:click="prev"
+          >
+            PREVIOUS
+          </button>
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white py-3 px-4 rounded"
+            v-on:click="next"
+          >
+            NEXT
+          </button>
         </div>
       </div>
-      <div v-show="questionIndex === questions.length">
+      <div
+        v-show="questionIndex === questions.length"
+        class="text-center mt-10"
+      >
         <h2>Quiz finished</h2>
         <p>Total score: {{ finished() }}</p>
+        <p v-if="finished() <= 4">You are Hero 1</p>
+        <p v-if="finished() > 4 && finished() <= 8">You are Hero 2</p>
+        <p v-if="finished() > 8 && finished() <= 12">You are Hero 3</p>
+        <p v-if="finished() > 12 && finished() <= 16">You are Hero 4</p>
+        <p v-if="finished() > 16">You are Hero 5</p>
+        <p>
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white py-3 px-4 rounded mt-10"
+            v-on:click="restart"
+          >
+            RESTART
+          </button>
+        </p>
       </div>
     </div>
-    <div v-else>
-      <button v-on:click="start">Start</button>
+    <div v-else class="text-center mt-10">
+      <button
+        class="bg-red-500 hover:bg-red-700 text-white py-3 px-4 rounded"
+        v-on:click="start"
+      >
+        START
+      </button>
     </div>
   </div>
 </template>
@@ -47,6 +87,7 @@ export default {
           id: 1,
           text: "Question 1",
           responses: [
+            { text: "Answer 0 Points", score: 0 },
             { text: "Answer 1 Point", score: 1 },
             { text: "Answer 2 Points", score: 2 },
             { text: "Answer 3 Points", score: 3 },
@@ -57,6 +98,7 @@ export default {
           id: 2,
           text: "Question 2",
           responses: [
+            { text: "Answer 0 Points", score: 0 },
             { text: "Answer 1 Point", score: 1 },
             { text: "Answer 2 Points", score: 2 },
             { text: "Answer 3 Points", score: 3 },
@@ -67,6 +109,7 @@ export default {
           id: 3,
           text: "Question 3",
           responses: [
+            { text: "Answer 0 Points", score: 0 },
             { text: "Answer 1 Point", score: 1 },
             { text: "Answer 2 Points", score: 2 },
             { text: "Answer 3 Points", score: 3 },
@@ -77,6 +120,7 @@ export default {
           id: 4,
           text: "Question 4",
           responses: [
+            { text: "Answer 0 Points", score: 0 },
             { text: "Answer 1 Point", score: 1 },
             { text: "Answer 2 Points", score: 2 },
             { text: "Answer 3 Points", score: 3 },
@@ -87,6 +131,7 @@ export default {
           id: 5,
           text: "Question 5",
           responses: [
+            { text: "Answer 0 Points", score: 0 },
             { text: "Answer 1 Point", score: 1 },
             { text: "Answer 2 Points", score: 2 },
             { text: "Answer 3 Points", score: 3 },
@@ -103,18 +148,21 @@ export default {
   methods: {
     start() {
       if (!this.userResponses[this.questionIndex]) {
-        this.userResponses[this.questionIndex] = 1;
+        this.userResponses[this.questionIndex] = 0;
       }
       this.started = true;
     },
     next() {
       this.questionIndex++;
       if (!this.userResponses[this.questionIndex]) {
-        this.userResponses[this.questionIndex] = 1;
+        this.userResponses[this.questionIndex] = 0;
       }
     },
     prev() {
       this.questionIndex--;
+    },
+    restart() {
+      location.reload();
     },
     finished() {
       let score = 0;
