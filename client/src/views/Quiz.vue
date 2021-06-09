74910ccce778daff86cb067230e26f68cfca8e8c
ReginaSchrattmaier <32732979+ReginaSchrattmaier@users.noreply.github.com>
2021-06-09 14:30:24 +0200

    add quiz questions and results, modify trending component for quiz result

--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -60,13 +60,61 @@
         v-show="questionIndex === questions.length"
         class="text-center mt-10"
       >
-        <h2>Quiz finished</h2>
-        <p>Total score: {{ finished() }}</p>
-        <p v-if="finished() <= 4">You are Hero 1</p>
-        <p v-if="finished() > 4 && finished() <= 8">You are Hero 2</p>
-        <p v-if="finished() > 8 && finished() <= 12">You are Hero 3</p>
-        <p v-if="finished() > 12 && finished() <= 16">You are Hero 4</p>
-        <p v-if="finished() > 16">You are Hero 5</p>
+        <div v-if="finished() <= 4" class="flex justify-center">
+          <trending-item
+            dataType="superhero"
+            :id="2048"
+            :name="'Wonder Woman'"
+            :imageURL="'https://comicvine.gamespot.com/a/uploads/scale_medium/12/124259/7823846-ekkbzkcli3kgfjkr3pfdxe-1280-80.jpg'"
+            :type="'quiz'"
+          />
+        </div>
+        <div
+          v-if="finished() > 4 && finished() <= 8"
+          class="flex justify-center"
+        >
+          <trending-item
+            dataType="superhero"
+            :id="1455"
+            :name="'Iron Man'"
+            :imageURL="'https://comicvine.gamespot.com/a/uploads/scale_medium/11118/111187046/7560188-0157851318-EeSKH3CWkAAiO2j.jpg'"
+            :type="'quiz'"
+          />
+        </div>
+        <div
+          v-if="finished() > 8 && finished() <= 12"
+          class="flex justify-center"
+        >
+          <trending-item
+            dataType="superhero"
+            :id="1807"
+            :name="'Superman'"
+            :imageURL="'https://comicvine.gamespot.com/a/uploads/scale_medium/13/132327/6507037-28872490_1638064799604695_1250122498385004714_n.jpg'"
+            :type="'quiz'"
+          />
+        </div>
+        <div
+          v-if="finished() > 12 && finished() <= 16"
+          class="flex justify-center"
+        >
+          <trending-item
+            dataType="superhero"
+            :id="1442"
+            :name="'Captain America'"
+            :imageURL="'https://comicvine.gamespot.com/a/uploads/scale_medium/5/57023/7391162-cap%20by%20alex%20ross.jpg'"
+            :type="'quiz'"
+          />
+        </div>
+        <div v-if="finished() > 16" class="flex justify-center">
+          <trending-item
+            dataType="superhero"
+            :id="2267"
+            :name="'Hulk'"
+            :imageURL="'https://comicvine.gamespot.com/a/uploads/scale_medium/12/124259/7892286-immortal_hulk_vol_1_38_.jpg'"
+            :type="'quiz'"
+          />
+        </div>
+
         <p>
           <button
             class="
@@ -97,9 +145,10 @@
 </template>
 
 <script>
+import TrendingItem from "../components/TrendingItem.vue";
 export default {
   name: "Quiz",
-  components: {},
+  components: { TrendingItem },
   props: {},
   watch: {},
   data() {
@@ -108,57 +157,57 @@ export default {
       questions: [
         {
           id: 1,
-          text: "Question 1",
+          text: "What do you think is the most important skill?",
           responses: [
-            { text: "Answer 0 Points", score: 0 },
-            { text: "Answer 1 Point", score: 1 },
-            { text: "Answer 2 Points", score: 2 },
-            { text: "Answer 3 Points", score: 3 },
-            { text: "Answer 4 Points", score: 4 },
+            { text: "Strength/Power", score: 0 },
+            { text: "The ability to control your anger", score: 4 },
+            { text: "Intelligence", score: 1 },
+            { text: "Combat", score: 3 },
+            { text: "Every skill is important", score: 2 },
           ],
         },
         {
           id: 2,
-          text: "Question 2",
+          text: "How would you describe yourself?",
           responses: [
-            { text: "Answer 0 Points", score: 0 },
-            { text: "Answer 1 Point", score: 1 },
-            { text: "Answer 2 Points", score: 2 },
-            { text: "Answer 3 Points", score: 3 },
-            { text: "Answer 4 Points", score: 4 },
+            { text: "I have only one weakness", score: 2 },
+            { text: "I am very loyal to my country", score: 3 },
+            { text: "I am a genius!", score: 1 },
+            { text: "I am quite independent", score: 0 },
+            { text: "I can be pretty strong in certain situations", score: 4 },
           ],
         },
         {
           id: 3,
-          text: "Question 3",
+          text: "Do you get angry often?",
           responses: [
-            { text: "Answer 0 Points", score: 0 },
-            { text: "Answer 1 Point", score: 1 },
-            { text: "Answer 2 Points", score: 2 },
-            { text: "Answer 3 Points", score: 3 },
-            { text: "Answer 4 Points", score: 4 },
+            { text: "If I do, it's a disaster", score: 4 },
+            { text: "Not really", score: 0 },
+            { text: "Yeah, it can happen", score: 3 },
+            { text: "Sometimes", score: 1 },
+            { text: "It depends", score: 2 },
           ],
         },
         {
           id: 4,
-          text: "Question 4",
+          text: "Do you like gadgets?",
           responses: [
-            { text: "Answer 0 Points", score: 0 },
-            { text: "Answer 1 Point", score: 1 },
-            { text: "Answer 2 Points", score: 2 },
-            { text: "Answer 3 Points", score: 3 },
-            { text: "Answer 4 Points", score: 4 },
+            { text: "They could be helpful", score: 3 },
+            { text: "I don't really need any", score: 4 },
+            { text: "A cape would be nice", score: 2 },
+            { text: "Yes, they can be nice", score: 0 },
+            { text: "Of course! I can’t live without them!", score: 1 },
           ],
         },
         {
           id: 5,
-          text: "Question 5",
+          text: "What is your biggest weakness?",
           responses: [
-            { text: "Answer 0 Points", score: 0 },
-            { text: "Answer 1 Point", score: 1 },
-            { text: "Answer 2 Points", score: 2 },
-            { text: "Answer 3 Points", score: 3 },
-            { text: "Answer 4 Points", score: 4 },
+            { text: "A certain type of mineral…", score: 2 },
+            { text: "Certain gadgets", score: 0 },
+            { text: "My ego", score: 3 },
+            { text: "My anger", score: 4 },
+            { text: "Myself", score: 1 },
           ],
         },
       ],
