aed7f7a6e32cf8ae111c9aa253d77c7c31d77b56
ReginaSchrattmaier <32732979+ReginaSchrattmaier@users.noreply.github.com>
2021-07-17 23:45:52 +0200

    add english version of about site

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -3,7 +3,7 @@
     <h1 class="uppercase text-3xl text-red-500 font-bold mb-4 text-center">
       About
     </h1>
-    <p class="text-center">
+    <p class="text-center mb-4">
       Diese Webseite entstand im Rahmen eines Semesterprojekts am<br />
       Masterstudiengang
 
@@ -16,8 +16,22 @@
       </span>
       der FH OÖ, Campus Hagenberg.
     </p>
+
+    <p class="text-center">
+      This website was created as a semester project in the <br />
+      master's program
+
+      <span class="underline">
+        <a
+          href="https://www.fh-ooe.at/en/hagenberg-campus/studiengaenge/master/interactive-media/"
+          target="_blank"
+          >Interactive Media</a
+        >
+      </span>
+      at the University of Applied Sciences in Hagenberg.
+    </p>
     <h2 class="uppercase text-3xl text-red-500 font-bold mt-4 mb-2 text-center">
-      Projektteam
+      Project Team
     </h2>
     <li class="mb-2 text-center list-none">Johannes Gerstbauer</li>
     <li class="mb-2 text-center list-none">Regina Schrattmaier</li>
